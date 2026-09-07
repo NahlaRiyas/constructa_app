0c15f7324d0159ccc34e7d7c30d96465d018f4e8
nahla <nahlariyas2002@gmail.com>
2026-09-07 10:50:57 +0530

    forget

--- lib/core/common/authentication_screens/forgot_password.dart
+++ lib/core/common/authentication_screens/forgot_password.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../../theme/palette.dart';
-import '../../../services/auth_service.dart';
+import '../../services/auth_service.dart';
 import '../utils/global.dart';
 
 
